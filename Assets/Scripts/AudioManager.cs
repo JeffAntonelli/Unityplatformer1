d42fad7669fd6d5f723ec2380bb64f00de446796
JeffAntonelli <antonellijeffrey@gmail.com>
2021-01-04 20:31:45 +0100

    add menu and game music

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip[] playlist;
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.clip = playlist[0];
+        audioSource.Play();
+    }
+
+}
